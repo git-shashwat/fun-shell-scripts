f2daa0849e1a3dcc9adafce10ce2817b293989c4
git-shashwat <shashwat824@gmail.com>
2020-12-16 10:36:49 +0530

    complete automation

--- cp_template_fetch.sh
+++ cp_template_fetch.sh
@@ -1,10 +1,30 @@
-code_file="main.cpp"
+function gocp() {
+	cd ~/Desktop/cp
+}
 
-if [[ ! -f "$code_file" ]];
-then
-	template=$(curl https://raw.githubusercontent.com/git-shashwat/Competitive-Programming/master/Template/temp.cpp)
-	>"$code_file"
-	echo "$template" > "$code_file"
-fi
+function createFolder() {
+	problem_code="$1"
+	
+	[ ! -d "$problem_code" ] && mkdir $problem_code
+		
+	cd $problem_code
+	
+	createFile
+}
 
-nano "$code_file"
+
+function createFile() {
+	code_file="main.cpp"
+
+	if [ ! -f "$code_file" ];
+	then
+		template=$(curl https://raw.githubusercontent.com/git-shashwat/Competitive-Programming/master/Template/temp.cpp)
+#		>"$code_file"
+		echo "$template" > "$code_file"
+	fi
+	
+	nohup gedit "$code_file" >/dev/null &
+}
+
+gocp
+createFolder $1
